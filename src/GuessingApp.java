2fede77cde9b5bf44fc85929b716e8e1631cd823
vishal singh <vishal1singh4444@gmail.com>
2026-02-07 12:42:36 +0530

    uc1 done

---
+++ src/GuessingApp.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class GuessingApp {
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO GUESSING GAME");
+        GameConfig gameConfig = new GameConfig();
+        gameConfig.showRules();
+    }
+}
